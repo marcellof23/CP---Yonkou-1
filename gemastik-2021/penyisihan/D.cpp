24993458eb9ccb51c9858b60575196cb84c9947a
jauhar-wibisono <13519160@std.stei.itb.ac.id>
2021-08-08 12:02:55 +0700

    add sol gemastik 2021

---
+++ gemastik-2021/penyisihan/D.cpp
@@ -0,0 +1,122 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long offset_pq(string s, long long m){
+    // s = 10eP+Q
+    long long p = 0, q = 0;
+    long long pls = 0;
+    while (s[pls] != '+') pls++;
+    for (long long i=3; i<pls; i++){
+        if (s[i] >= '0' && s[i] <= '9'){
+            p *= 10;
+            p += s[i]-'0';
+        }
+    }
+    for (long long i=pls+1; i<(long long)s.length(); i++){
+        if (s[i] >= '0' && s[i] <= '9'){
+            q *= 10;
+            q += s[i]-'0';
+        }
+    }
+    long long ret = 1, mul = 10;
+    // cout << p << ' ' << q << '\n';
+    for (; p; p>>=1){
+        if (p&1){
+            ret *= mul;
+            ret %= m;
+        }
+        mul *= mul;
+        mul %= m;
+    }
+    ret += q;
+    ret %= m;
+    return ret;
+}
+
+void tes(){
+    while (1){
+        string s; long long m;
+        cin >> s >> m;
+        cout << offset_pq(s, m) << '\n';
+    }
+    /*
+    10e2+3 104
+    10e0+0 10000
+    10e1000+2222 13
+    12222
+    */
+}
+
+int main(){
+    // tes();
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    long long n, k, l;
+    string sl;
+    long long q;
+    cin >> n;
+    string s[n], cpy[n], dummy;
+    for(long long i=0; i<n; i++) cin >> s[i];
+    cin >> q;
+    for (long long qq=0; qq<q; qq++){
+        //Mulai cycle
+        cin >> k >> sl;
+        long long source, target[n];
+        bool vis[n];
+        for(long long i=0; i<n; i++){
+            vis[i] = false;
+            target[i] = i;
+        }
+        while(k--){
+            cin >> source >> dummy;
+            source--;
+            cin >> target[source];
+            target[source]--;
+
+        }
+        for(long long i=0; i<n; i++){
+            if(!vis[i]){
+                vector<long long> cycle;
+                long long cur = i;
+                cycle.push_back(cur);
+                vis[cur] = true;
+                while(target[cur]!=i){
+                    cur = target[cur];
+                    vis[cur] = true;
+                    cycle.push_back(cur);
+                }
+                long long sz = cycle.size();
+                if (sl.length() < 3 || sl[2] != 'e') l = stoll(sl)%sz;
+                else l = offset_pq(sl, sz);
+                // cout << "l = " << l << '\n';
+                for(long long j=0; j<sz; j++){
+                    cpy[cycle[(l + j)%sz]] = s[cycle[j]];
+                }
+            }
+        }
+        for(long long i=0; i<n; i++){
+            cout << cpy[i] << " ";
+        }
+        cout << '\n';
+    }
+}
+
+/*
+6
+time space reality power soul mind
+3
+4 1
+1 -> 2
+2 -> 1
+4 -> 6
+6 -> 4
+2 13
+2 -> 4
+4 -> 2
+6 10e6 + 4
+1 -> 2
+2 -> 3
+3 -> 4
+4 -> 5
+5 -> 6
+6 -> 1
+*/
