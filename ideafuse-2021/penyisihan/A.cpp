fc1f1939033f48d8a84ab9c2990fa9e0e4a44cc3
Marcello Faria <01faria.marcello@gmail.com>
2021-08-22 12:29:42 +0700

    add sol

---
+++ ideafuse-2021/penyisihan/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T, N;
+  cin >> T;
+  for (int t = 1; t <= T; t++)
+  {
+    cin >> N;
+    int arr[N], sortedArr[N];
+    for (int i = 0; i < N; i++)
+    {
+      cin >> arr[i];
+      sortedArr[i] = arr[i];
+    }
+    sort(sortedArr, sortedArr + N);
+    int cnt = 0;
+    for (int i = 0; i < N; i++)
+    {
+      if (arr[i] != sortedArr[i])
+        cnt++;
+    }
+    cout << "Case #" << t << ": " << cnt << endl;
+  }
+}
