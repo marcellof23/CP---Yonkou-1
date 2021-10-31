24d4f3629083139fa7e81572679962b3866074c7
Marcello Faria <01faria.marcello@gmail.com>
2021-10-31 09:58:41 +0700

    add dummy folder

--- INC2021/trial/PB.cpp
+++ INC2021/trial/PB.cpp
@@ -29,44 +29,30 @@ int main()
     curr_row = 0;
     for (int j = 0; j < N; j++)
     {
+      if (j == 0)
+      {
+        curr_col = 0;
+      }
       if (curr_row < grid[i][j])
       {
         curr_row = grid[i][j];
         rows[i]++;
       }
-    }
-  }
-
-  for (int j = 0; j < N; j++)
-  {
-    curr_col = 0;
-    for (int i = 0; i < N; i++)
-    {
-      if (curr_col < grid[i][j])
+      if (curr_col < grid[j][i])
       {
-        curr_col = grid[i][j];
-        cols[j]++;
+        curr_col = grid[j][i];
+        cols[i]++;
       }
     }
   }
 
   for (int i = 0; i < N; i++)
   {
-    if (cols[i] == 0)
-    {
-      cout << 1 << " ";
-    }
-    else
-      cout << cols[i] << " ";
+    cout << cols[i] << " ";
   }
   cout << endl;
   for (int i = 0; i < N; i++)
   {
-    if (rows[i] == 0)
-    {
-      cout << 1 << endl;
-    }
-    else
-      cout << rows[i] << endl;
+    cout << rows[i] << endl;
   }
 }
