47bef6402f7ef4361fa2ede5c0ff22e05aaa88cc
Marcello Faria <01faria.marcello@gmail.com>
2021-12-19 21:35:13 +0700

    add template

--- latianICPC2021/2021-2020 Saint-Petersburg/A.cpp
+++ latianICPC2021/2021-2020 Saint-Petersburg/A.cpp
@@ -6,5 +6,33 @@ int main()
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
+  int n, m, rook;
+  cin >> n >> m >> rook;
+  int maks = min(n, m);
+  if (maks < rook)
+  {
+    cout << "Impossible" << endl;
+  }
+  else
+  {
+    cout << "Possible" << endl;
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        if (j == i && rook != 0)
+        {
+          cout << "*";
+          rook--;
+        }
+        else
+        {
+          cout << ".";
+        }
+      }
+      cout << endl;
+    }
+  }
+
   return 0;
 }
